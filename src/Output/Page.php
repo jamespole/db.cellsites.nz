4b58f71f02e7d244f31224a188cbd28f1639429f
James Anderson-Pole <james@pole.net.nz>
2026-01-30 21:53:25 +1300

    Update Page.php

--- src/Output/Page.php
+++ src/Output/Page.php
@@ -21,8 +21,8 @@ abstract class Page extends Output
         $string .= '<title>Cell Sites Database</title>' . PHP_EOL;
         $string .= '<link rel="stylesheet" href="/style.css">' . PHP_EOL;
         if ($this->requiresLeaflet === true) {
-            $string .= '<link rel="stylesheet" href="/leaflet/leaflet.css">' . PHP_EOL;
-            $string .= '<script src="/leaflet/leaflet.js"></script>' . PHP_EOL;
+            $string .= '<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>' . PHP_EOL;
+            $string .= '<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>' . PHP_EOL;
             $string .= '<link rel="stylesheet" href="/leaflet.fullscreen/Control.FullScreen.css">' . PHP_EOL;
             $string .= '<script src="/leaflet.fullscreen/Control.FullScreen.js"></script>' . PHP_EOL;
         }

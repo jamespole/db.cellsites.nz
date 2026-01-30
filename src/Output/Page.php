4e01080f2f068795b123ed769891058bedf806de
James Anderson-Pole <james@pole.net.nz>
2026-01-30 22:05:45 +1300

    Update Page.php

--- src/Output/Page.php
+++ src/Output/Page.php
@@ -23,8 +23,8 @@ abstract class Page extends Output
         if ($this->requiresLeaflet === true) {
             $string .= '<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>' . PHP_EOL;
             $string .= '<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>' . PHP_EOL;
-            $string .= '<link rel="stylesheet" href="/leaflet.fullscreen/Control.FullScreen.css">' . PHP_EOL;
-            $string .= '<script src="/leaflet.fullscreen/Control.FullScreen.js"></script>' . PHP_EOL;
+            $string .= '<link rel="stylesheet" href="https://unpkg.com/leaflet.fullscreen@5.3.0/dist/Control.FullScreen.css" />' . PHP_EOL;
+            $string .= '<script src="https://unpkg.com/leaflet.fullscreen@5.3.0/dist/Control.FullScreen.umd.js"></script>' . PHP_EOL;
         }
         $string .= '</head>' . PHP_EOL;
         $string .= '<body>' . PHP_EOL;

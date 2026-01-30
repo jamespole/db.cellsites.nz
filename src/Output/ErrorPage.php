d2f15dac426bd8532e68b7a1c3792f239395b2e5
James Anderson-Pole <james@pole.net.nz>
2026-01-30 20:16:00 +1300

    Create ErrorPage.php

---
+++ src/Output/ErrorPage.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JamesPole\DbCellsitesNz\Output;
+
+final class ErrorPage extends Page
+{
+    private int $error;
+    public function __construct(int $error)
+    {
+        parent::__construct();
+        $this->error = $error;
+    }
+    protected function generateBody(): string
+    {
+        $this->setResponseCode($this->error);
+        $string = '<h2>Error ' . $this->error . '</h2>' . PHP_EOL;
+        if (isset($_SERVER['REQUEST_URI'])) {
+            $string .= '<p><b>REQUEST_URI:</b> ' . $_SERVER['REQUEST_URI'] . '</p>' . PHP_EOL;
+        }
+        return($string);
+    }
+}

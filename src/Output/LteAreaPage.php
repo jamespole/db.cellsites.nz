f6ca0fc2f97193be4d56e7c045076da30a952148
James Anderson-Pole <james@pole.net.nz>
2026-02-01 17:06:36 +1300

    Update LteAreaPage.php

--- src/Output/LteAreaPage.php
+++ src/Output/LteAreaPage.php
@@ -37,30 +37,7 @@ final class LteAreaPage extends Page
         }
         $string = $this->generateBreadcrumbs();
         if (count($this->nodes) !== 0) {
-            $string .= '<h3>Map of nodes</h3>' . PHP_EOL;
-            $string .= '<div id="map" style="height:40em"></div>' . PHP_EOL;
-            $string .= '<script>' . PHP_EOL;
-            $string .= 'const map = L.map(\'map\');' . PHP_EOL;
-            $string .= 'map.addControl(new L.Control.FullScreen());' . PHP_EOL;
-            $string .= 'var markers = [' . PHP_EOL;
-            foreach ($this->nodes as $thisNode) {
-                $site = $thisNode->getSite();
-                if ($site === null) {
-                    continue;
-                }
-                $string .= sprintf(
-                    ' L.marker([%s]),' . PHP_EOL,
-                    (string)$site->getLocation()->getCoordinate()->format(new DecimalDegrees(','))
-                );
-            }
-            $string .= '];' . PHP_EOL;
-            $string .= 'var group = L.featureGroup(markers).addTo(map);' . PHP_EOL;
-            $string .= 'map.fitBounds(group.getBounds());' . PHP_EOL;
-            $string .= 'L.tileLayer(\'https://tile.openstreetmap.org/{z}/{x}/{y}.png\', {' . PHP_EOL;
-            $string .= ' maxZoom: 19,' . PHP_EOL;
-            $string .= ' attribution: \'&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>\'' . PHP_EOL;
-            $string .= '}).addTo(map);' . PHP_EOL;
-            $string .= '</script>' . PHP_EOL;
+            $string .= $this->generateMap();
             $string .= sprintf(
                 '<h3>Nodes <small>(%d nodes)</small></h3>' . PHP_EOL,
                 count($this->nodes)
@@ -99,7 +76,7 @@ final class LteAreaPage extends Page
     private function generateBreadcrumbs(): string
     {
         $string = '<nav aria-label="breadcrumb">' . PHP_EOL;
-        $string .= '<ol class="breadcrumb mt-3">' . PHP_EOL;
+        $string .= '<ol class="breadcrumb m-3">' . PHP_EOL;
         $string .= '<li class="breadcrumb-item"><a href="/">Home</a></li>' . PHP_EOL;
         $string .= sprintf(
             '<li class="breadcrumb-item"><a href="/country/%s">%s</a></li>' . PHP_EOL,
@@ -107,7 +84,7 @@ final class LteAreaPage extends Page
             htmlentities($this->area->getNetwork()->getCountry()->getName())
         );
         $string .= sprintf(
-            '<li class="breadcrumb-item"><a href="/netwwork/%s">%s</a></li>' . PHP_EOL,
+            '<li class="breadcrumb-item"><a href="/network/%s">%s</a></li>' . PHP_EOL,
             htmlentities((string)$this->area->getNetwork()->getUuid()),
             htmlentities($this->area->getNetwork()->getName())
         );
@@ -117,6 +94,34 @@ final class LteAreaPage extends Page
         );
         $string .= '</ol>' . PHP_EOL;
         $string .= '</nav>' . PHP_EOL;
+        return($string);
+    }
+    private static function generateMap(): string
+    {
+        $string = '<h3>Map of nodes</h3>' . PHP_EOL;
+        $string .= '<div id="map" style="height:40em"></div>' . PHP_EOL;
+        $string .= '<script>' . PHP_EOL;
+        $string .= 'const map = L.map(\'map\');' . PHP_EOL;
+        $string .= 'map.addControl(new L.Control.FullScreen());' . PHP_EOL;
+        $string .= 'var markers = [' . PHP_EOL;
+        foreach ($this->nodes as $thisNode) {
+            $site = $thisNode->getSite();
+            if ($site === null) {
+                continue;
+            }
+            $string .= sprintf(
+                ' L.marker([%s]),' . PHP_EOL,
+                (string)$site->getLocation()->getCoordinate()->format(new DecimalDegrees(','))
+            );
+        }
+        $string .= '];' . PHP_EOL;
+        $string .= 'var group = L.featureGroup(markers).addTo(map);' . PHP_EOL;
+        $string .= 'map.fitBounds(group.getBounds());' . PHP_EOL;
+        $string .= 'L.tileLayer(\'https://tile.openstreetmap.org/{z}/{x}/{y}.png\', {' . PHP_EOL;
+        $string .= ' maxZoom: 19,' . PHP_EOL;
+        $string .= ' attribution: \'&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>\'' . PHP_EOL;
+        $string .= '}).addTo(map);' . PHP_EOL;
+        $string .= '</script>' . PHP_EOL;
         return($string);
     }
     private static function generateNotFound(): string

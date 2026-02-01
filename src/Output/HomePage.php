70e228338a445649cf2544b9ae43dbd7bdbb739a
James Anderson-Pole <james@pole.net.nz>
2026-02-01 15:34:47 +1300

    Update HomePage.php

--- src/Output/HomePage.php
+++ src/Output/HomePage.php
@@ -30,7 +30,7 @@ final class HomePage extends Page
                 '<a class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" href="/country/%s">' . PHP_EOL,
                 htmlentities((string)$thisCountry->getUuid())
             );
-            $string .= sprintf('<span>%s</span>' . PHP_EOL, htmlentities($thisCountry->getName()));
+            $string .= sprintf('<span class="fs-1">%s</span>' . PHP_EOL, htmlentities($thisCountry->getName()));
             $string .= sprintf('<span class="text-secondary">%03d</span>' . PHP_EOL, $thisCountry->getMcc());
             $string .= '</a>' . PHP_EOL;
         }
